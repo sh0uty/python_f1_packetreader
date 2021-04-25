2aec24fd4f94e1c61e2fbacd1e75937928d07c49
sh0uty <lukeom6@web.de>
2021-04-25 17:17:26 +0200

    main.py help information from argparse edited

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import logging
 from threading import Thread
 
 
-parser = argparse.ArgumentParser(prog="F1-2020-Telemetry udp reciever", description="Collects telemetry data from F1 2020 udp server")
-parser.add_argument("-i", "--ip", help="IP where F1-2020 runs", required=True)
+parser = argparse.ArgumentParser(prog="F1-2020-Telemetry udp receiver", description="Collects telemetry data from F1 2020 udp server")
+parser.add_argument("-i", "--ip", help="Local ip address or listen on all interfaces e.g. 0.0.0.0", required=True)
 parser.add_argument("-p", "--port", help="Port of the udp server. Default: 20777", type=int, default=20777)
 
 args = parser.parse_args()
